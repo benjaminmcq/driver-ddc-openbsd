2679bb71e11fd2688a5893af0ac72ee542c07af2
Benjamin Lee McQueen <mcq@disroot.org>
2026-07-30 17:32:48 -0500

    add ddc.h

---
+++ ddc.h
@@ -0,0 +1,9 @@
+#include <ddc.c>
+
+#define DDC_CMD_CAPS            0xF3
+#define DDC_HOST_ADDR_ODD       0x51
+#define DDC_HOST_ADDR_EVEN      0x50
+#define DDC_DEFAULT_DEVICE_ADDR 0x6E
+#define DDC_HOST_REPLY_CAPS     0xE3
+#define DDC_MONITOR_ADDR        0x37
+#define DDC_PFLAG               0x80
