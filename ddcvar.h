ec0f2e62c7a2f01fd91d744f3bd9cbacf35ca5a1
Benjamin Lee McQueen <mcq@disroot.org>
2026-08-03 17:21:42 -0500

    Add function prototypes + more
    Added function prototypes for 
    ddc_register,
    ddc_unregister,
    ddc_probe_device // I THINK I NEED THIS FOR WHEN WE EXPOSE AN IOCTL
    
    added includes so the function prototypes are ok
    as well as adding an #ifndef guard to make sure it's only included once.

--- ddcvar.h
+++ ddcvar.h
@@ -14,10 +14,23 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#define DDC_CMD_CAPS            0xF3
-#define DDC_HOST_ADDR_ODD       0x51
-#define DDC_HOST_ADDR_EVEN      0x50
-#define DDC_DEFAULT_DEVICE_ADDR 0x6E
-#define DDC_HOST_REPLY_CAPS     0xE3
-#define DDC_MONITOR_ADDR        0x37
-#define DDC_PFLAG               0x80
+#ifndef _SYS_DEV_DDCVAR_H_
+#define _SYS_DEV_DDCVAR_H_
+
+#include <sys/device.h>
+#include <dev/i2c/i2cvar.h>
+#include <linux/i2c.h>
+
+int   ddc_register(struct device *, struct i2c_adapter *, i2c_addr_t);
+void  ddc_unregister(struct device *);
+int   ddc_probe_device(struct device *, struct i2c_adapter *);
+
+#define DDC_CMD_CAPS             0xF3
+#define DDC_HOST_ADDR_ODD        0x51
+#define DDC_HOST_ADDR_EVEN       0x50
+#define DDC_DEFAULT_DEVICE_ADDR  0x6E
+#define DDC_HOST_REPLY_CAPS      0xE3
+#define DDC_MONITOR_ADDR         0x37
+#define DDC_PFLAG                0x80
+
+#endif /* _SYS_DEV_DDCVAR_H_ */
